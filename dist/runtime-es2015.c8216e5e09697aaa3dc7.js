f782d4e7904e66d9d64e57b31ef2b6661f7d8ed4
Kyaw Zin Thant <kyawzinucsm89@gmail.com>
2022-04-20 02:21:20 +0630

    fix bug:

---
+++ dist/runtime-es2015.c8216e5e09697aaa3dc7.js
@@ -0,0 +1,224 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// install a JSONP callback for chunk loading
+/******/ 	function webpackJsonpCallback(data) {
+/******/ 		var chunkIds = data[0];
+/******/ 		var moreModules = data[1];
+/******/ 		var executeModules = data[2];
+/******/
+/******/ 		// add "moreModules" to the modules object,
+/******/ 		// then flag all "chunkIds" as loaded and fire callback
+/******/ 		var moduleId, chunkId, i = 0, resolves = [];
+/******/ 		for(;i < chunkIds.length; i++) {
+/******/ 			chunkId = chunkIds[i];
+/******/ 			if(Object.prototype.hasOwnProperty.call(installedChunks, chunkId) && installedChunks[chunkId]) {
+/******/ 				resolves.push(installedChunks[chunkId][0]);
+/******/ 			}
+/******/ 			installedChunks[chunkId] = 0;
+/******/ 		}
+/******/ 		for(moduleId in moreModules) {
+/******/ 			if(Object.prototype.hasOwnProperty.call(moreModules, moduleId)) {
+/******/ 				modules[moduleId] = moreModules[moduleId];
+/******/ 			}
+/******/ 		}
+/******/ 		if(parentJsonpFunction) parentJsonpFunction(data);
+/******/
+/******/ 		while(resolves.length) {
+/******/ 			resolves.shift()();
+/******/ 		}
+/******/
+/******/ 		// add entry modules from loaded chunk to deferred list
+/******/ 		deferredModules.push.apply(deferredModules, executeModules || []);
+/******/
+/******/ 		// run deferred modules when all chunks ready
+/******/ 		return checkDeferredModules();
+/******/ 	};
+/******/ 	function checkDeferredModules() {
+/******/ 		var result;
+/******/ 		for(var i = 0; i < deferredModules.length; i++) {
+/******/ 			var deferredModule = deferredModules[i];
+/******/ 			var fulfilled = true;
+/******/ 			for(var j = 1; j < deferredModule.length; j++) {
+/******/ 				var depId = deferredModule[j];
+/******/ 				if(installedChunks[depId] !== 0) fulfilled = false;
+/******/ 			}
+/******/ 			if(fulfilled) {
+/******/ 				deferredModules.splice(i--, 1);
+/******/ 				result = __webpack_require__(__webpack_require__.s = deferredModule[0]);
+/******/ 			}
+/******/ 		}
+/******/
+/******/ 		return result;
+/******/ 	}
+/******/
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// object to store loaded and loading chunks
+/******/ 	// undefined = chunk not loaded, null = chunk preloaded/prefetched
+/******/ 	// Promise = chunk loading, 0 = chunk loaded
+/******/ 	var installedChunks = {
+/******/ 		"runtime": 0
+/******/ 	};
+/******/
+/******/ 	var deferredModules = [];
+/******/
+/******/ 	// script path function
+/******/ 	function jsonpScriptSrc(chunkId) {
+/******/ 		return __webpack_require__.p + "" + ({"default~app-modules-ai-store-ai-store-module~app-modules-live-class-live-class-module~app-modules-ma~112d434e":"default~app-modules-ai-store-ai-store-module~app-modules-live-class-live-class-module~app-modules-ma~112d434e","app-modules-ai-store-ai-store-module":"app-modules-ai-store-ai-store-module","app-modules-live-class-live-class-module":"app-modules-live-class-live-class-module","app-modules-manage-live-class-live-class-module":"app-modules-manage-live-class-live-class-module","app-modules-manage-manage-questions-manage-questions-module":"app-modules-manage-manage-questions-manage-questions-module","app-modules-manage-manage-questions-question-create-question-create-module":"app-modules-manage-manage-questions-question-create-question-create-module","app-modules-manage-packages-packages-module":"app-modules-manage-packages-packages-module","app-modules-manage-promotion-promotion-module":"app-modules-manage-promotion-promotion-module","app-modules-manage-training-game-training-game-module":"app-modules-manage-training-game-training-game-module","app-modules-manage-users-user-module":"app-modules-manage-users-user-module","app-modules-myaccount-myaccount-module":"app-modules-myaccount-myaccount-module","app-modules-training-evaluate-training-evaluate-module":"app-modules-training-evaluate-training-evaluate-module","app-modules-training-game-training-game-module":"app-modules-training-game-training-game-module","common":"common","app-modules-manage-individual-mock-individual-mock-module":"app-modules-manage-individual-mock-individual-mock-module","app-modules-manage-practise-practise-module":"app-modules-manage-practise-practise-module","app-modules-manage-predictions-predictions-module":"app-modules-manage-predictions-predictions-module","app-modules-manage-study-plans-study-plans-module":"app-modules-manage-study-plans-study-plans-module","modules-full-mock-test-full-mock-test-module":"modules-full-mock-test-full-mock-test-module","modules-mainView-main-view-module":"modules-mainView-main-view-module","modules-manage-manage-module":"modules-manage-manage-module","modules-manage-mock-test-mock-test-module":"modules-manage-mock-test-mock-test-module","modules-questions-questions-module":"modules-questions-questions-module","default~app-modules-other-evaluate-other-evaluate-module~app-modules-other-question-module-other-que~ba6936d6":"default~app-modules-other-evaluate-other-evaluate-module~app-modules-other-question-module-other-que~ba6936d6","app-modules-other-evaluate-other-evaluate-module":"app-modules-other-evaluate-other-evaluate-module","app-modules-practise-evaluate-practise-evaluate-module":"app-modules-practise-evaluate-practise-evaluate-module","app-modules-question-evaluate-question-evaluate-module":"app-modules-question-evaluate-question-evaluate-module","app-modules-other-question-module-other-question-module-module":"app-modules-other-question-module-other-question-module-module","modules-home-home-module":"modules-home-home-module"}[chunkId]||chunkId) + "-es2015." + {"default~app-modules-ai-store-ai-store-module~app-modules-live-class-live-class-module~app-modules-ma~112d434e":"25482b701211d61f9154","app-modules-ai-store-ai-store-module":"9522be3cd8d00fa13292","app-modules-live-class-live-class-module":"2e8aee775341571960a9","app-modules-manage-live-class-live-class-module":"19a02d6a986ee7760e91","app-modules-manage-manage-questions-manage-questions-module":"a3a31d0b3df1bf35323c","app-modules-manage-manage-questions-question-create-question-create-module":"1604567d3260c1b10792","app-modules-manage-packages-packages-module":"8cfc3165a63d288cd85d","app-modules-manage-promotion-promotion-module":"bb3c8d9e518f8b7043a4","app-modules-manage-training-game-training-game-module":"2c7cd8fdadc96371b634","app-modules-manage-users-user-module":"b1accd98d5b879e70de0","app-modules-myaccount-myaccount-module":"304d2b17b217d73fe571","app-modules-training-evaluate-training-evaluate-module":"5763e6b3aacc649ee7a8","app-modules-training-game-training-game-module":"c08a98ae16719e352212","common":"bf60ee412bdb4252f244","app-modules-manage-individual-mock-individual-mock-module":"4743553d4d896c2b8c6e","app-modules-manage-practise-practise-module":"5c3de98c3b2066aba9b4","app-modules-manage-predictions-predictions-module":"cf9a9dccc26ca5d690a9","app-modules-manage-study-plans-study-plans-module":"df9ac905dfc81dac4e63","modules-full-mock-test-full-mock-test-module":"9cca22c64138c6adfca2","modules-mainView-main-view-module":"fa22356d099a2018f133","modules-manage-manage-module":"3cbf563032861dd660da","modules-manage-mock-test-mock-test-module":"e304cdde115af24f20f7","modules-questions-questions-module":"a88a2517bfd40f4e56e8","default~app-modules-other-evaluate-other-evaluate-module~app-modules-other-question-module-other-que~ba6936d6":"c14c58a6373fb4e4a4f8","app-modules-other-evaluate-other-evaluate-module":"8bea003745af7f65436e","app-modules-practise-evaluate-practise-evaluate-module":"57ebe5818323cac0b09a","app-modules-question-evaluate-question-evaluate-module":"daf781adbeb6f33801b7","app-modules-other-question-module-other-question-module-module":"4660949a4e6ef0a999f6","modules-home-home-module":"fce83480d3edaf1bbe1f"}[chunkId] + ".js"
+/******/ 	}
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/ 	// This file contains only the entry chunk.
+/******/ 	// The chunk loading function for additional chunks
+/******/ 	__webpack_require__.e = function requireEnsure(chunkId) {
+/******/ 		var promises = [];
+/******/
+/******/
+/******/ 		// JSONP chunk loading for javascript
+/******/
+/******/ 		var installedChunkData = installedChunks[chunkId];
+/******/ 		if(installedChunkData !== 0) { // 0 means "already installed".
+/******/
+/******/ 			// a Promise means "currently loading".
+/******/ 			if(installedChunkData) {
+/******/ 				promises.push(installedChunkData[2]);
+/******/ 			} else {
+/******/ 				// setup Promise in chunk cache
+/******/ 				var promise = new Promise(function(resolve, reject) {
+/******/ 					installedChunkData = installedChunks[chunkId] = [resolve, reject];
+/******/ 				});
+/******/ 				promises.push(installedChunkData[2] = promise);
+/******/
+/******/ 				// start chunk loading
+/******/ 				var script = document.createElement('script');
+/******/ 				var onScriptComplete;
+/******/
+/******/ 				script.charset = 'utf-8';
+/******/ 				script.timeout = 120;
+/******/ 				if (__webpack_require__.nc) {
+/******/ 					script.setAttribute("nonce", __webpack_require__.nc);
+/******/ 				}
+/******/ 				script.src = jsonpScriptSrc(chunkId);
+/******/
+/******/ 				// create error before stack unwound to get useful stacktrace later
+/******/ 				var error = new Error();
+/******/ 				onScriptComplete = function (event) {
+/******/ 					// avoid mem leaks in IE.
+/******/ 					script.onerror = script.onload = null;
+/******/ 					clearTimeout(timeout);
+/******/ 					var chunk = installedChunks[chunkId];
+/******/ 					if(chunk !== 0) {
+/******/ 						if(chunk) {
+/******/ 							var errorType = event && (event.type === 'load' ? 'missing' : event.type);
+/******/ 							var realSrc = event && event.target && event.target.src;
+/******/ 							error.message = 'Loading chunk ' + chunkId + ' failed.\n(' + errorType + ': ' + realSrc + ')';
+/******/ 							error.name = 'ChunkLoadError';
+/******/ 							error.type = errorType;
+/******/ 							error.request = realSrc;
+/******/ 							chunk[1](error);
+/******/ 						}
+/******/ 						installedChunks[chunkId] = undefined;
+/******/ 					}
+/******/ 				};
+/******/ 				var timeout = setTimeout(function(){
+/******/ 					onScriptComplete({ type: 'timeout', target: script });
+/******/ 				}, 120000);
+/******/ 				script.onerror = script.onload = onScriptComplete;
+/******/ 				document.head.appendChild(script);
+/******/ 			}
+/******/ 		}
+/******/ 		return Promise.all(promises);
+/******/ 	};
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// on error function for async loading
+/******/ 	__webpack_require__.oe = function(err) { console.error(err); throw err; };
+/******/
+/******/ 	var jsonpArray = window["webpackJsonp"] = window["webpackJsonp"] || [];
+/******/ 	var oldJsonpFunction = jsonpArray.push.bind(jsonpArray);
+/******/ 	jsonpArray.push = webpackJsonpCallback;
+/******/ 	jsonpArray = jsonpArray.slice();
+/******/ 	for(var i = 0; i < jsonpArray.length; i++) webpackJsonpCallback(jsonpArray[i]);
+/******/ 	var parentJsonpFunction = oldJsonpFunction;
+/******/
+/******/
+/******/ 	// run deferred modules from other chunks
+/******/ 	checkDeferredModules();
+/******/ })
+/************************************************************************/
+/******/ ([]);
+//# sourceMappingURL=runtime-es2015.c8216e5e09697aaa3dc7.js.map
